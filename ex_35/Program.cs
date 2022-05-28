cfa2297fe9f32a968d10249e4f124f0412a5d885
ELIZA <rysaeva96@mail.ru>
2022-05-28 10:40:21 +0500

    Решение 35 задачи

---
+++ ex_35/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте одномерный массив из 123 случайных чисел
+// найти количество элементов массива, значения
+// которых лежат в отрезке [10, 99]
+
+int [] nums = new int [12];
+var numRnd = new Random();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums [i] = numRnd.Next(0, 500);
+    Console.Write(nums[i] + " ");
+}
+Console.WriteLine();
+int j = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] >= 10 && nums[i] < 100)
+    {
+        j++;
+    }
+}
+Console.WriteLine();
+Console.Write($"Количество элементов массива в отрезке {j}");
+Console.WriteLine();
+
